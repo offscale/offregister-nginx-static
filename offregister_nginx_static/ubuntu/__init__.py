794d12497d3ba9df6714e995abe205f2cc2ebb9e
Samuel Marks <samuelmarks@gmail.com>
2017-11-20 17:17:35 +1100

    Made `conf_remote_filename` an argument

--- offregister_nginx_static/ubuntu/__init__.py
+++ offregister_nginx_static/ubuntu/__init__.py
@@ -18,9 +18,9 @@ def setup_conf0(nginx_conf='api_and_static.conf', conf_keys=None, skip_nginx_res
     conf_local_filepath = kwargs.get(
         'nginx-conf-file', resource_filename('offregister_nginx_static', path.join('conf', nginx_conf))
     )
-    conf_remote_filename = '/etc/nginx/conf.d/{}'.format(
+    conf_remote_filename = kwargs.get('conf_remote_filename', '/etc/nginx/conf.d/{}'.format(
         kwargs.get('nginx-conf-filename', path.basename(conf_local_filepath))
-    )
+    ))
     upload_template(conf_local_filepath, conf_remote_filename,
                     context=conf_keys if conf_keys is None else {k: kwargs[k] for k in conf_keys},
                     use_sudo=True)
