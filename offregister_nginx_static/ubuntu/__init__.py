d11583379b9a59486f3978b7c30e91ec1e56203a
Samuel Marks <samuelmarks@gmail.com>
2017-11-20 17:11:25 +1100

    Proper multiple conf support (with interpolation); new sample template; renamed old template; using systemd import

--- offregister_nginx_static/ubuntu/__init__.py
+++ offregister_nginx_static/ubuntu/__init__.py
@@ -5,22 +5,29 @@ from fabric.contrib.files import upload_template
 from fabric.operations import sudo
 
 from offregister_fab_utils.apt import apt_depends
+from offregister_fab_utils.ubuntu.systemd import restart_systemd
 
 
-def setup_conf0(*args, **kwargs):
+def setup_conf0(nginx_conf='api_and_static.conf', conf_keys=None, skip_nginx_restart=False, *args, **kwargs):
     apt_depends('nginx')
-    conf_local_filepath = kwargs.get('nginx-conf-file',
-                                     resource_filename('offregister_nginx_static', path.join('conf', 'nginx.conf')))
+
+    if conf_keys is None:
+        conf_keys = {'api_and_static.conf': ('SERVER_NAME', 'WWWROOT', 'API_HOST', 'API_PORT'),
+                     'static.conf': ('SERVER_NAME', 'WWWROOT')}.get(nginx_conf)
+
+    conf_local_filepath = kwargs.get(
+        'nginx-conf-file', resource_filename('offregister_nginx_static', path.join('conf', nginx_conf))
+    )
     conf_remote_filename = '/etc/nginx/conf.d/{}'.format(
         kwargs.get('nginx-conf-filename', path.basename(conf_local_filepath))
     )
     upload_template(conf_local_filepath, conf_remote_filename,
-                    # context={'SERVER_NAME': kwargs['SERVER_NAME'], 'SERVER_LOCATION': kwargs['SERVER_LOCATION']},
+                    context=conf_keys if conf_keys is None else {k: kwargs[k] for k in conf_keys},
                     use_sudo=True)
-    if sudo('systemctl status -q nginx --no-pager --full', warn_only=True).failed:
-        sudo('systemctl start -q nginx --no-pager --full')
-    else:
-        sudo('systemctl stop -q nginx --no-pager --full')
-        sudo('systemctl start -q nginx --no-pager --full')
+
+    if skip_nginx_restart:
+        return
+
+    restart_systemd('nginx')
 
     return sudo('systemctl status nginx --no-pager --full')
